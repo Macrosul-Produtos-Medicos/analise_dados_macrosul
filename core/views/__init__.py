b2227ff440e86db12e46aa00d0090bee001a75cf
giordano <giordano.silveira@gmail.com>
2025-12-17 09:24:36 -0300

    feat: reorganize logistica views into package structure

---
+++ core/views/__init__.py
@@ -0,0 +1,16 @@
+# Base view
+from .base import BaseProtectedView
+
+# Dashboard views
+from .dashboard_views import (
+    IndexView,
+    TabelaExemploView,
+    DashboardEstoqueView,
+    ConsultaEquipamentosPecasView,
+    TicketMedioView,
+)
+
+# Logistica views
+from .logistica_views import (
+    ListarRelacaoNotasView,
+)
