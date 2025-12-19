3610edea3afa8fe19fbc6ae5e03685bbd55ceb75
giordano <giordano.silveira@gmail.com>
2025-12-19 14:45:31 -0300

    fix: Corrigindo a url da api

--- sistema_bom/urls.py
+++ sistema_bom/urls.py
@@ -1,9 +1,11 @@
 from django.contrib import admin
 from django.urls import path, include
 
+from .api import api
+
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('core.urls')),
-    path('api/v1/', include('api.urls')),
+    path('api/v1/', api.urls),
     path('accounts/', include('allauth.urls')),
 ]
